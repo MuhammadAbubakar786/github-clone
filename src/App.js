119a8b71cdb85ccb434a5b6c9a7099b3445d4b11
alihassan <iamali62426@gmail.com>
2021-11-25 07:48:50 -0800

    folder structure updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TrendingRepo from './pages/TrendingRepo';
 
 function App() {
   return (
     <div className="App">
-
+              <TrendingRepo />
     </div>
   );
 }
---
+++ src/pages/TrendingRepo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Badge } from 'react-bootstrap';
+
+export default function TrendingRepo() {
+    return (
+        <h1>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h1>
+    )
+}
